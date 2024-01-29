406875a1850c370c3d43b02a3a26f026f82cb653
VictorMaeda <victor.chinen@eaportal.org>
2024-01-29 16:54:21 -0400

    Inicio

---
+++ src/main/java/Desafios/Itau/DesafioTransancaoItauApplication.java
@@ -0,0 +1,13 @@
+package Desafios.Itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTransancaoItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTransancaoItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Desafios/Itau/DesafioTransancaoItauApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafios.Itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTransancaoItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
